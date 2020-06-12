a8937bb3015fe6dcca3ddd4d10a327ca3104e990
mtorres9095 <mtorres9095@gmail.com>
2020-06-12 07:45:17 -0400

    Fixed Job requirements Part I

--- TechJobsOO/Job.cs
+++ TechJobsOO/Job.cs
@@ -5,7 +5,7 @@ namespace TechJobsOO
     {
         public int Id { get; }
         private static int nextId = 1;
-        private static int jobIdCounter = 1;
+        private static int jobId = 1;
 
         public int JobId { get; set; }
         public string Name { get; set; }
@@ -25,19 +25,19 @@ namespace TechJobsOO
         }
         public Job()
         {
-            JobId = jobIdCounter;
-            jobIdCounter++;
+            JobId = jobId;
+            jobId++;
         }
 
         // TODO: Generate Equals() and GetHashCode() methods.
         public override bool Equals(object obj)
         {
-            return obj is Job.jobType && Id == employer.Id;//Check,
+            return obj is Job job && JobId == job.JobId;//Check,
         }
 
         public override int GetHashCode()
         {
-            return HashCode.Combine(Id);
+            return HashCode.Combine(JobId);
         }
 
     }
