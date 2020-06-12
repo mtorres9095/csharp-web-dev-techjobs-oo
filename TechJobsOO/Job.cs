e0df36e6dff7bc8be327ca642d57813c4ea3fb53
mtorres9095 <mtorres9095@gmail.com>
2020-06-11 23:06:05 -0400

    Last Changes for the Night

--- TechJobsOO/Job.cs
+++ TechJobsOO/Job.cs
@@ -5,7 +5,9 @@ namespace TechJobsOO
     {
         public int Id { get; }
         private static int nextId = 1;
+        private static int jobIdCounter = 1;
 
+        public int JobId { get; set; }
         public string Name { get; set; }
         public Employer EmployerName { get; set; }
         public Location EmployerLocation { get; set; }
@@ -13,7 +15,30 @@ namespace TechJobsOO
         public CoreCompetency JobCoreCompetency { get; set; }
 
         // TODO: Add the two necessary constructors.
+        public Job(string name, string employerName, string employerLocation, string jobType, string jobCoreCompetency)
+        {
+            Name = name;
+            EmployerName = employerName;
+            EmployerLocation = employerLocation;
+            JobType = jobType;
+            JobCoreCompetency = jobCoreCompetency;
+        }
+        public Job()
+        {
+            JobId = jobIdCounter;
+            jobIdCounter++;
+        }
 
         // TODO: Generate Equals() and GetHashCode() methods.
+        public override bool Equals(object obj)
+        {
+            return obj is Job.jobType && Id == employer.Id;//Check,
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+
     }
 }
---
+++ TechJobsTests/JobTests.cs
@@ -0,0 +1,48 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TechJobsTests
+{
+    [TestClass]
+    public class JobTests
+    {
+        //[TestMethod]
+        //public void TestMethod1()
+        //{
+        //}
+        //[TestMethod]
+        //public void TestSettingJobId()
+        //{
+
+        //}
+        [TestMethod]
+        public void TestJobIDCounter1()//1 Empty
+        {
+            Job testingID1 = new Job();
+            Assert.AreEqual(1, testingID1.JobId);
+        }
+        [TestMethod]
+        public void TestJobIDCounter2()//2 Empty
+        {
+            Job testingID2 = new Job();
+            Assert.AreEqual(2, testingID2.JobId);
+        }
+
+        [TestMethod]
+        public void TestJobConstructorSetsAllFields()
+        {
+            Job testConstructor = new Job("Product tester", "ACME", "Desert", "Quality control", "Persistence");
+            Assert.AreEqual("Product tester", testConstructor.Name);
+            Assert.AreEqual("ACME", testConstructor.EmployerName);
+            Assert.AreEqual("Desert", testConstructor.EmployerLocation);
+            Assert.AreEqual("Quality control", testConstructor.JobType);
+            Assert.AreEqual("Persistence", testConstructor.JobCoreCompetency);
+        }
+        [TestMethod]
+        public void TestJobsForEquality()
+        {
+            
+            Assert.IsFalse
+        }
+    }
+}
